8141372b3e71ce75f769982a98c08136cb2fd3ef
LAVISH KUMAR <44172938+lavishkumar0125@users.noreply.github.com>
2023-02-15 17:04:27 +0530

    second commit

--- studentInfo.py
+++ studentInfo.py
@@ -2,31 +2,61 @@ import pandas as pd
 import numpy as np
 
 
-# get whole student information from excel by creating dataframe
-def getStudentInfo():
-    student_info = pd.read_excel('Students.xlsx')
-    # print(type(studentInfo))
-    # print(student_info)
-    return student_info
+# get student data from excel
+
+def getStudentData():
+    s_data = pd.read_excel('Students.xlsx')
+    return s_data
 
 
-sInfo = getStudentInfo()
-# print(sInfo)
+# print(getStudentData())
+
+# get names of students from excel
+
+def getStudentNames():
+    s_data = getStudentData()
+    names = s_data['Name']
+
+    return names
 
 
-def getStudentMarks():
-    df = getStudentInfo()
+# print(getStudentNames())
+
+# get students current college year
+
+
+def getStudentsYear():
+    s_data = getStudentData()
+    years = list(s_data.Year)
+    names = list(s_data.Name)
+    total_marks_year = {}
+    for name in names:
+        for year in years:
+            total_marks_year[name] = {'Year': year}
+    return total_marks_year
+
+
+# print(getStudentsYear())
+
+
+# get yearly marks of students
+
+def getStudentsMarks():
+    df = getStudentData()
     marks = df[['Name', 'First_Year',
                 'Second_Year', 'Third_Year', 'Fourth_Year']]
     return marks
 
-# marks = getStudentMarks()
-# print(marks)
 
+# print(getStudentsMarks())
+
+
+# get total marks of students till previous year
 
 def getTotalMarks():
-    df = getStudentInfo()
-    dict = {}
+    df = getStudentData()
+    df = df.fillna(value=0)
+    total_marks = {}
     for index, row in df.iterrows():
         name = row['Name']
         marks_first = row['First_Year']
@@ -34,48 +64,98 @@ def getTotalMarks():
         marks_third = row['Third_Year']
         marks_fourth = row['Fourth_Year']
         marks = marks_first+marks_second+marks_third+marks_fourth
-        dict[name] = marks
-    return dict
+        total_marks[name] = marks
+    return total_marks
 
+
+# print("Total marks are:\n")
+# print(getTotalMarks())
+
+
+# get percentage of each student
 
 def getPercentage():
-    dict = getTotalMarks()
-    for name, marks in dict.items():
-        percentage = ((marks/400)*100)
-        dict[name] = percentage
-    return dict
+    total_marks = getTotalMarks()
+    s_data = getStudentData()
+    years = list(s_data.Year)
+    i = 0
+    percentage = {}
+    for name, marks in total_marks.items():
+        year = years[i]
+        percent = ((marks/(year*100))*100)
+        percentage[name] = round((float(percent)), 2)
+        i += 1
+    return percentage
 
 
-percentage = getPercentage()
-# print(percentage)
+# print("percentage is:\n")
+# print(getPercentage())
+
+
+# set the percentage in excel file
+
+
+def setPercentage():
+    percentage = getPercentage()
+    df = getStudentData()
+    for name, percent in percentage.items():
+        for index, row in df.iterrows():
+            name = row['Name']
+            if name in percentage:
+                percent = percentage[name]
+                df.loc[index, 'Percentage'] = percent
+    df.to_excel('Students.xlsx', index=False)
+    return True
+
+
+# set_percenrage = setPercentage()
+# print(set_percenrage)
+
+
+# get names of students whose percentage is more than 90
+
+def getTopStudents():
+    s_data = getStudentData()
+    percentage = s_data[s_data['Percentage'] > 90]
+    topStudent = percentage[['Name', 'Percentage']]
+    return topStudent
+
+
+# print(getTopStudents())
+
+
+# get students details using roll no
+
+
+def getStudentDetails(rollNo):
+    s_data = getStudentData()
+    s_details = s_data[s_data['Roll_No'] ==
+                       rollNo][['Name', 'Email_ID', 'Age']]
+
+    return s_details
+
+
+roll_no = int(input("Enter Roll no: "))
+print(getStudentDetails(roll_no))
 
 
 def gethighestMarks():
     pass
 
 
-def getHighestMarksOfStudent():
+def getHighestMarksEachYear():
     pass
+
+
+# change email id of a student
 
 
 def changeEmailid():
     pass
 
 
-def changeMarks():
+# change marks of students of given year
+
+
+def changeMarks(name, year, newMarks):
     pass
-
-
-def setPercentage(percentage):
-    df = getStudentInfo()
-    percentage = getPercentage()
-    # for name , percent in percentage.items()
-    for index, row in df.iterrows():
-        name = row['Name']
-        if name in percentage:
-            percent = percentage[name]
-            df.loc[index, 'Percentage'] = percent
-    df.to_excel('Students.xlsx', index=False)
-
-
-s = setPercentage(percentage)
